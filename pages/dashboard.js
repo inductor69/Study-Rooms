22e52ebff8f63866b10b625f5fed18555f089247
inductor69 <adityakiitdelhi@gmail.com>
2022-01-08 16:39:37 +0530

    update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,15 @@
-import DashBoard from "../components/dashboard/dashboard"
-import Layout from '../components/Layout'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout1 from '../components/Layout1'
+import theme from '../styles/theme'
 
-export default function dashboard() {
-
-
-    return (
-        <Layout isDashBoard={true} >
-            <DashBoard/>
-
-        </Layout>
-    )
+function Dashboard({ Component, pageProps }) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Layout1>
+        <Component {...pageProps} />
+      </Layout1>
+    </ChakraProvider>
+  )
 }
+
+export default Dashboard
